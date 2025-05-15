61bcd29f9d475e5d1c539c4ee671e9af06563036
tusharkewat <tusharkewat1234@gmail.com>
2025-05-15 19:54:02 +0530

    Day 9

---
+++ Array/Number_of_Occurrences.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Number_of_Occurrences {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+        System.out.println("Enter " + size + " elements : ");
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        System.out.println("Enter your target : ");
+        int target = sc.nextInt();
+        for(int i=0;i<size;i++){
+            if(arr[i] == target){
+                count++;
+            }
+        }
+        System.out.println("Occurrences of "+target+" is : "+count);
+        sc.close();
+    }
+}
